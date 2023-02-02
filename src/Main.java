2581d622572941a6779f47b5761fddf56dce4e0d
Ella <you@example.com>
2023-02-02 20:56:59 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 3400;
+        int rubMili = 20;
+        System.out.println("Количество начисленных миль:");
+        System.out.println((amount / rubMili + "миль"));
+        int totalMili = amount / rubMili;
+
+
+    }
+}
